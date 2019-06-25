7696c5731b33f57e41a46e868ae6f6d6623df92b
shengyang <shengyang@bj-m-200191c.local>
2019-06-25 12:35:05 +0800

    去掉多余执行命令

--- wvs/test/autotest.sh
+++ wvs/test/autotest.sh
@@ -5,13 +5,11 @@ if [ ! -n "$DIR" ] ;then
     exit 
 fi 
 
-#php easyswoole stop php easyswoole start --d 
 fswatch $DIR | while read file 
 do 
     #echo "${file} was modify" >> unittest.log 2>&1 
     echo "${file} was modify" 
     pytest -v -s -m"scan" ${file} 
-    #php easyswoole reload 
 done
 
 
